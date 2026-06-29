b6415f2203624921d809dd965adbe758a7329db4
YuraIvaniv <37239062+Yuralviv@users.noreply.github.com>
2026-06-29 14:06:51 +0300

    test deploy

--- test/global-setup.ts
+++
@@ -1,7 +0,0 @@
-import { existsSync, renameSync } from 'fs';
-
-export default async () => {
-  if (existsSync('.env')) {
-    renameSync('.env', '.env.temp');
-  }
-};
--- test/global-teardown.ts
+++
@@ -1,7 +0,0 @@
-import { existsSync, renameSync } from 'fs';
-
-export default async () => {
-  if (existsSync('.env.temp')) {
-    renameSync('.env.temp', '.env');
-  }
-};
